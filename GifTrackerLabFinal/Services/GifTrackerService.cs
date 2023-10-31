79272b9fa017630e81719ed980d682469446354f
iworsham <15iworsham@gmail.com>
2023-10-31 15:52:44 -0400

    for joe

--- GifTrackerLabFinal/Services/GifTrackerService.cs
+++ GifTrackerLabFinal/Services/GifTrackerService.cs
@@ -1,9 +1,14 @@
 ﻿using GifTrackerLabFinal.Interfaces;
+using GifTrackerLabFinal.Models;
 
 
 namespace GifTrackerLabFinal.Services
 {
     public class GifTrackerService : IGifTrackerService
     {
+        public Task<List<GifTracker>> GetGif(string name, string url, int rating)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
