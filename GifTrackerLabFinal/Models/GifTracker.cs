3e949329f6f12d56cfd4108001caa378437af88d
abukhmirov <alexbukhmirov@gmail.com>
2023-10-31 13:36:16 -0600

    Rest of the bunches of stuff

--- GifTrackerLabFinal/Models/GifTracker.cs
+++ GifTrackerLabFinal/Models/GifTracker.cs
@@ -2,5 +2,9 @@
 {
     public class GifTracker
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public int Rating { get; set; }
     }
 }
